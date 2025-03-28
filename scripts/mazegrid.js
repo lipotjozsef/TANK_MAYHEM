2568d208525677fe7b0f71931668958b33a22dd8
Bruckner Artur <bruckner.artur.benedek@students.jedlik.eu>
2025-03-28 11:06:35 +0100

    maze kirajzolás

---
+++ scripts/mazegrid.js
@@ -0,0 +1,10 @@
+export class MazeGrid{
+    constructor(top, right, bottom, left, width, height) {
+        this.top = top
+        this.right = right
+        this.bottom = bottom
+        this.left = left
+        this.width = width
+        this.height = height
+    }
+}
