b8649e3340ccd7ab870696f570d822604231dd23
Bruckner Artur <bruckner.artur.benedek@students.jedlik.eu>
2025-04-09 09:27:20 +0200

    powerup images complete

--- scripts/powerup.js
+++ scripts/powerup.js
@@ -3,7 +3,8 @@ export class PowerUp{
     
     constructor(canvas, X, Y, radius, scale, padding, typeNumber = 0) {
 
-        this.typeList = ["explode", "shield", "laser"]
+        this.typeList = ["shield", "laser", "explosion", "rocket"]
+        this.backgroundcolorList = ["yellow", "red", "green", "lightblue"]
         this.type = this.typeList[typeNumber]
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
@@ -12,6 +13,9 @@ export class PowerUp{
         this.radius = radius,
         this.scale = scale
         this.padding = padding
+        this.image = new Image();
+        this.image.src = "assets/" + this.type + ".png";
+        this.backgroundcolor = this.backgroundcolorList[typeNumber] 
     }
 
     spawnPowerUp()
@@ -21,15 +25,15 @@ export class PowerUp{
         this.ctx.lineWidth = 5
         this.ctx.arc(this.x * this.scale + this.radius + this.padding, this.y * this.scale + this.radius + this.padding, this.radius, 0, 2 * Math.PI)
         this.ctx.stroke();
-        this.ctx.fillStyle = "yellow"
+        this.ctx.fillStyle =this.backgroundcolor
         this.ctx.fill()
-        this.ctx.fillStyle = "black"
-        this.ctx.fillText(this.type, this.x * this.scale + this.radius - 25 / 4, this.y * this.scale + (this.radius * 1.4), 25)
+        this.ctx.drawImage(this.image, this.x * this.scale + this.radius * 0.25, this.y * this.scale + this.radius * 0.25, this.radius * 2, this.radius * 2);
     }
 
     disspawnPowerUp()
     {
         this.ctx.fillStyle = "white"
         this.ctx.fillRect(this.x * this.scale, this.y * this.scale, this.scale, this.scale)
+        return this.type
     }
 }
