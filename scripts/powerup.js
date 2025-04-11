824eff7f99c94e36752b7406c51b4d36f426f50d
Bruckner Artur <bruckner.artur.benedek@students.jedlik.eu>
2025-04-11 10:32:15 +0200

    powerup IsPIckedUp

--- scripts/powerup.js
+++ scripts/powerup.js
@@ -16,24 +16,30 @@ export class PowerUp{
         this.image = new Image();
         this.image.src = "assets/" + this.type + ".png";
         this.backgroundcolor = this.backgroundcolorList[typeNumber] 
+        this.isPickedUp = false
     }
 
     spawnPowerUp()
     {
-        this.ctx.beginPath();
-        this.ctx.strokeStyle = "black"
-        this.ctx.lineWidth = 5
-        this.ctx.arc(this.x * this.scale + this.radius + this.padding, this.y * this.scale + this.radius + this.padding, this.radius, 0, 2 * Math.PI)
-        this.ctx.stroke();
-        this.ctx.fillStyle =this.backgroundcolor
-        this.ctx.fill()
-        this.ctx.drawImage(this.image, this.x * this.scale + this.radius * 0.25, this.y * this.scale + this.radius * 0.25, this.radius * 2, this.radius * 2);
+        if(!this.isPickedUp)
+        {
+
+            this.ctx.beginPath();
+            this.ctx.strokeStyle = "black"
+            this.ctx.lineWidth = 5
+            this.ctx.arc(this.x * this.scale + this.radius + this.padding, this.y * this.scale + this.radius + this.padding, this.radius, 0, 2 * Math.PI)
+            this.ctx.stroke();
+            this.ctx.fillStyle =this.backgroundcolor
+            this.ctx.fill()
+            this.ctx.drawImage(this.image, this.x * this.scale + this.radius * 0.25, this.y * this.scale + this.radius * 0.25, this.radius * 2, this.radius * 2);
+        }
     }
 
     disspawnPowerUp()
     {
         this.ctx.fillStyle = "white"
         this.ctx.fillRect(this.x * this.scale, this.y * this.scale, this.scale, this.scale)
+        this.isPickedUp = true
         return this.type
     }
 }
